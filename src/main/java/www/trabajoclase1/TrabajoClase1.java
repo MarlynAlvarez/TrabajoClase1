f1397b90d4591cad2dda74138ea8cbdaf35a1054
MarlynÁlvarez <alvarezmarlyn42@gmail.com>
2025-10-01 14:41:36 -0500

    creacion de objetos

--- src/main/java/www/trabajoclase1/TrabajoClase1.java
+++ src/main/java/www/trabajoclase1/TrabajoClase1.java
@@ -5,7 +5,13 @@ package www.trabajoclase1;
 public class TrabajoClase1 {
 
     public static void main(String[] args) {
-       
+        Estudiantes estudianteUno = new Estudiantes();
+        estudianteUno.nombre = "Marlyn";
+        estudianteUno.identificacion = 220032351;
+        estudianteUno.promedio = 7 ;
+        System.out.println("nombre:"+ estudianteUno.nombre);
+        System.out.println("identificacion:"+ estudianteUno.identificacion);
+        System.out.println("promedio:"+ estudianteUno.promedio);
     }
 }
  
