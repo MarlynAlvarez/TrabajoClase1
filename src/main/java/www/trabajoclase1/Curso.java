ef1f84015dc78089bdb80eff72b4ab9de123f4b2
MarlynÁlvarez <alvarezmarlyn42@gmail.com>
2025-10-01 10:34:53 -0500

    practica de las clases

---
+++ src/main/java/www/trabajoclase1/Curso.java
@@ -0,0 +1,24 @@
+
+package www.trabajoclase1;
+
+public class Curso {
+
+   //atributos
+    public String nombreCurso;
+    public String codigo;
+    public float pendiente;
+    
+    //metodos
+    public void agregarEstudiante(){
+        
+    }
+    
+    public void eliminar(){
+        
+    }
+    
+    public void pendiente(){
+        
+    }
+   
+}
---
+++ src/main/java/www/trabajoclase1/Estudiante.java
@@ -0,0 +1,24 @@
+
+package www.trabajoclase1;
+
+public class Estudiante {
+    // Atributos
+    public String nombre;
+    public String identificacion;
+    public float promedio;
+    
+    //metodos
+    public void inscripcion(){
+        
+    }
+    
+    public void retiro(){   
+        
+    }
+    
+    public void mostrarInformscion(){
+        
+    }
+    
+    
+}
